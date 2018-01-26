e10fa7e158a078d0ca876d10823cd128fc6240a2
Geoff Bourne <itzgeoff@gmail.com>
2018-01-26 10:24:35 -0600

    Add logback receiver config and leverage spring properties

--- src/main/java/me/itzg/logbackkafkarelay/LogbackKafkaRelayApplication.java
+++ src/main/java/me/itzg/logbackkafkarelay/LogbackKafkaRelayApplication.java
@@ -1,13 +1,22 @@
 package me.itzg.logbackkafkarelay;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication @Slf4j
 public class LogbackKafkaRelayApplication implements ApplicationRunner {
+
+    private final Environment env;
+
+    @Autowired
+    public LogbackKafkaRelayApplication(Environment env) {
+        this.env = env;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogbackKafkaRelayApplication.class, args);
@@ -15,7 +24,10 @@ public class LogbackKafkaRelayApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments applicationArguments) throws Exception {
-		log.info("Running relay");
+		log.info("Running relay. Logback receiver listening={}:{}, Kafka bootstrap servers={}",
+                 env.getProperty("logback.receiver.address"),
+                 env.getProperty("logback.receiver.port"),
+                 env.getProperty("spring.kafka.bootstrap-servers"));
 		Thread.currentThread().join();
 	}
 }
